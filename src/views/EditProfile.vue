3f64e28101be8b6b880e418ce90d1bf1d96a4940
Birkis <blomstergutt@gmail.com>
2020-02-02 16:45:11 +0100

    Edit Profile Layout Changes

--- src/views/EditProfile.vue
+++ src/views/EditProfile.vue
@@ -1,22 +1,125 @@
 <template>
-<v-content class="" >
+<v-content class="" > 
+    <v-layout justify-center>
+        <h1 class="display-1" justify-center>Edit Your Profile</h1>
+    </v-layout>   
+    <v-container>
+       <v-form v-model="valid"> 
+            <v-layout row wrap justify-space-around>
+                <v-flex xs12 md5> <!-- Name & Email input Collapsed Code -->
+                            <v-container> <!-- Diverse input fields -->
+                                <v-layout row wrap>   <!-- firstname, lastname, email -->
+                                    <v-text-field
+                                        v-model="firstname"
+                                        :rules="nameRules"
+                                        :counter="10"
+                                        label="First name"
+                                        required
+                                    ></v-text-field>   <!-- firstname -->                        
+                                    <v-text-field
+                                        v-model="lastname"
+                                        :rules="nameRules"
+                                        :counter="10"
+                                        label="Last name"
+                                        required
+                                    ></v-text-field> <!-- lastname -->
+                                    <v-text-field
+                                        v-model="email"
+                                        :rules="emailRules"
+                                        label="E-mail"
+                                        required
+                                    ></v-text-field> <!-- email -->
+                                </v-layout>
+                                <v-combobox
+                                v-model="gender_model"
+                                :items="gender_options"
+                                :search-input.sync="gender_search"
+                                hide-selected
+                                hint="Set your gender"
+                                label="Where do you identify?"
+                                persistent-hint
+                                small-chips
+                                >  <!-- gender -->
+                                    <template v-slot:no-data>
+                                        <v-list-item>
+                                        <v-list-item-content>
+                                            <v-list-item-title>
+                                            No results matching "<strong>{{ search }}</strong>". Press <kbd>enter</kbd> to create a new one
+                                            </v-list-item-title>
+                                        </v-list-item-content>
+                                        </v-list-item>
+                                    </template>
+                                </v-combobox>
+                            </v-container>
+                            <v-container fluid> <!--  Add some Tags Collapsed code  -->
+                                <v-combobox
+                                                v-model="model"
+                                                :items="items"
+                                                :search-input.sync="search"
+                                                hide-selected
+                                                hint="Maximum of 5 tags"
+                                                label="Add some tags to your profile"
+                                                multiple
+                                                persistent-hint
+                                                small-chips
+                                                >
+                                                <template v-slot:no-data>
+                                                    <v-list-item>
+                                                    <v-list-item-content>
+                                                        <v-list-item-title>
+                                                        No results matching "<strong>{{ search }}</strong>". Press <kbd>enter</kbd> to create a new one
+                                                        </v-list-item-title>
+                                                    </v-list-item-content>
+                                                    </v-list-item>
+                                                </template>
+                                </v-combobox>
+                            </v-container>  
+                            <v-container>  <!-- DOB Picker -->
+                                <v-menu
+                                    ref="menu"
+                                    v-model="menu"
+                                    :close-on-content-click="false"
+                                    :return-value.sync="dob"
+                                    transition="scale-transition"
+                                    offset-y
+                                    min-width="290px"
+                                    >
+                                    <template v-slot:activator="{ on }">
+                                            <v-text-field
+                                            v-model="dob"
+                                            label="Set your date of birth"
+                                            readonly
+                                            v-on="on"
+                                            ></v-text-field>
+                                    </template>
+                                    <v-date-picker v-model="dob" no-title scrollable>
+                                        <v-spacer></v-spacer>
+                                        <v-btn text color="primary" @click="menu = false">Cancel</v-btn>
+                                        <v-btn text color="primary" @click="$refs.menu.save(dob)">OK</v-btn>
+                                    </v-date-picker>
+                                </v-menu>
+                            </v-container>               
+                </v-flex>    
+                <v-flex xs12 md5> <!-- Add Bio -->
+                            <v-textarea
+                                name="input-7-1"
+                                label="Bio"
+                                placeholder="Fill this space with something ingenious"
+                                hint="Write a few words about yourself"
+                                ></v-textarea>
+                            <v-text-field
+                                        v-model="motto"
+                                        :rules="mottoRules"
+                                        :counter="60"
+                                        label="motto"
+                            ></v-text-field> 
 
-    <h1 class="display-1">Edit Your Profile</h1>
-    <v-container class="my-5">
-            <v-layout row wrap>
-                <v-flex xs12 md6>
-                    <p class="font-italic font-weight-light">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente qui vero quia accusantium, ad eum est sit excepturi! Fugiat vero dolorem impedit, qui eaque voluptates neque numquam at tenetur cumque!</p>
-                </v-flex>
-                <v-flex xs12 md6>
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe accusamus dignissimos doloremque reiciendis odit explicabo, nihil aliquid nostrum praesentium voluptas laborum ut numquam laboriosam corrupti tenetur. Mollitia voluptatum molestias ducimus.</p>
-                </v-flex>
 
+                </v-flex>
             </v-layout>
-        
-         </v-container>
-    
-</v-content>
-  
+        </v-form>
+    </v-container>  
+</v-content> 
 </template>
 
 
@@ -26,7 +129,50 @@ export default {
     data(){
         return{
 
+            items: ['indoor', 'outdoor', 'gym', 'fitness','professional','amateur','noob','enthusiast','team sports','mindfulness'],
+            model: [],
+            search: null,
+            valid: false,
+            menu:false,
+            firstname: '',
+            lastname: '',
+            nameRules: [
+                v => !!v || 'Name is required',
+                v => v.length <= 10 || 'Name must be less than 10 characters',
+            ],
+            mottoRules: [
+                v => v.length <= 60 || 'Motto must be less than 40 characters',
+            ],
+            dob: new Date().toISOString().substr(0, 10),
+            located:null,
+            gender_options:['Male','Female','None of yo business','Somewhere in between'],
+            gender_model:[],
+            gender_search: null,
+            gender_valid:false,
+            email: '',
+            emailRules: [
+                v => !!v || 'E-mail is required',
+                v => /.+@.+/.test(v) || 'E-mail must be valid',
+            ],
+            isTrainer:false,
+            motto:'',
+            trainsAt:[],
+            reviews:[ {rating:5, description:'Elsket det!',reviewer:'Jens Jensen'} ]
+
         }
-    }
+    },
+    watch: {
+      model (val) {
+        if (val.length > 5) {
+          this.$nextTick(() => this.model.pop())
+        }
+      },
+    },
 }
 </script>
+
+<style scoped>
+
+
+
+</style>
